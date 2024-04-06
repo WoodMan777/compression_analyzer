c3c93a2519f654180519386c85b341ff1fbf1488
WoodMan777 <wdmfrs@gmail.com>
2024-04-06 23:38:06 +0300

    Added sqlite3 dbs for saving data. Added cmd lines generator and yaml config file parser

--- src/main.py
+++ src/main.py
@@ -1,43 +1,71 @@
-import tools
-import os
-import hashlib
+import tools, os, hashlib, sqlite3
+import time
 
-folderpath = "D:\\Compression\\data_to_compress\\"
+
+folderpath = "C:\\Users\\WDMFR\\Desktop\\Лабы\\4H1\\Compression\\data_to_compress\\"
+outpath = "C:\\Users\\WDMFR\\Desktop\\Лабы\\4H1\\Compression\\compressed\\"
 files = os.listdir(folderpath)
+exepath = "C:\\Users\\WDMFR\\Desktop\\Лабы\\4H1\\Compression\\7z2301-x64.exe"
 
 print(f"Chosen path: {folderpath}")
 print(f"Found {len(files)} files\n")
 
+metricdb = sqlite3.connect('metric.db')
+cursorm = metricdb.cursor()
+cursorm.execute("""
+        CREATE TABLE IF NOT EXISTS Metric (
+        id INTEGER PRIMARY KEY,
+        hash TINYTEXT,
+        value DOULBE
+        )
+        """)
 
-if "metricdb" not in os.listdir():
-    print("metricdb wasn't found")
-    metricdb = open("metricdb", "w+")
-else:
-    metricdb = open("metricdb", "a+")
 
-if "compressiondb" not in os.listdir():
-    print("compressiondb wasn't found")
-    compressiondb = open("compressiondb", "w+")
-else:
-    compressiondb = open("compressiondb", "a+")
+
+compressiondb = sqlite3.connect('comression.db')
+cursord = compressiondb.cursor()
+cursord.execute("""
+        CREATE TABLE IF NOT EXISTS Data (
+        id INTEGER PRIMARY KEY,
+        parameters TEXT,
+        time DOUBLE,
+        size BIGINT,
+        csize BIGINT,
+        hash TINYTEXT,
+        name TINYTEXT
+        )
+        """)
 
 
 print("Checking if randomness for selected files has been already calculated")
 
 for file in files:
     print(f"Checking file {file} ({files.index(file)}/{len(files)})")
-    filebin = open(folderpath + file, "rb", buffering = 0)
-    tmphash = hashlib.file_digest(filebin, 'sha512').hexdigest()
+    tmphash = tools.calculateHash(folderpath + file)
     filebin = open(folderpath + file, "rb", buffering = 0)
     filebin = filebin.read()
-    if tmphash in metricdb:
+    cursorm.execute('SELECT value FROM Metric WHERE hash = ?',(tmphash,))
+    res = cursorm.fetchall()
+    if len(res):
         print(f"found {file} in database")
     else:
         print(f"Randomness calculation wasn't found in database. Calculating...")
         matrixtmp = tools.getFastMarkovMatrix(filebin)
         metric = tools.getMarkovRandomness(matrixtmp)
-        metricdb.write(f"{tmphash},{metric}\n")
+        cursorm.execute('INSERT INTO Metric (hash, value) VALUES (?, ?)',(tmphash, metric))
+        metricdb.commit()
         print(f"Calculated randomness for {file}.")
 
-        
+allcmds = tools.generateAllCmdStrings()
 
+for command in allcmds:
+    for file in files:
+        cmd = f"{command} {outpath}{file}.7z {folderpath}{file}"
+        start = time.time()
+        os.system(cmd)
+        end = time.time()
+        orig_size = os.stat(f"{folderpath}{file}").st_size
+        comp_size = os.stat(f"{folderpath}{file}.7z").st_size
+        tmphash = tools.calculateHash(folderpath+file)
+        cursord.execute('''INSERT INTO Metric (parameters, time, size, csize, hash, name) 
+                        VALUES (?, ?, ?, ?, ?, ?)''', (command, start-end, orig_size, comp_size, tmphash, file))
--- src/tools.py
+++ src/tools.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from time import time
 import bitstring as bs
+import yaml
 
 def getFastMarkovMatrix(binfile):
     count = np.zeros((2**8, 2**8), dtype = "float64")
@@ -37,3 +38,47 @@ def getMarkovRandomness(matrix):
         if i >= threshold:
             metric += i
     return metric
+
+def cartesianMultiplication(a,b):
+    res = []
+    for i in a:
+        for j in b:
+            res.append(i+' '+j)
+    return res
+
+def generateAllCmdStrings(exepath, ymlfile):
+    res = []
+    for test in ymlfile.keys():
+        params = {}
+        for param in ymlfile[test].keys():
+            if 'bool' in str(type(ymlfile[test][param])):
+                params[param] = True
+            elif ',' in ymlfile[test][param] and '-' in ymlfile[test][param]:
+                print("ERR: One (or few) parameters contain ',' and '-' at the same time!")
+                return None
+            elif ',' in ymlfile[test][param]:
+                tmp = ymlfile[test][param].split(',')
+                tmp = list(map(lambda x: param+x, tmp))
+                params[param] = tmp
+            elif '-' in ymlfile[test][param]:
+                tmp = ymlfile[test][param].split('-')
+                tmp2 = []
+                for i in range(int(tmp[0]),int(tmp[1])+1):
+                    tmp2.append(f"{param}{i}")
+                params[param] = tmp2
+        
+        singular = []
+        for param in params.keys():
+            if params[param] == True:
+                singular.append(param)
+        
+        singular = ' '.join(singular)
+        singular = [singular]
+        
+        for param in params.keys():
+            if params[param] != True:
+                singular = cartesianMultiplication(singular, params[param])
+        
+        res += singular
+    res = list(map(lambda x: exepath+" "+x, res))
+    return res
